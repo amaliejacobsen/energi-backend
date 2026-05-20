203081ae67b1ddd5755eba9019bd57f6de2b75b9
amaliejacobsen <amajac@norlys.dk>
2026-05-20 08:42:35 +0200

    Update collector.py

--- collector.py
+++ collector.py
@@ -1063,7 +1063,7 @@ def collect_temperature_forecast_data():
 
 def collect_all():
     print(f"\n{'='*40}\nStart: {datetime.now()}\n{'='*40}")
-    collect_temperature_forecast_data()
+    collect_dk_data()
     print(f"\nFærdig: {datetime.now()}\n{'='*40}")
 
 if __name__ == "__main__":
