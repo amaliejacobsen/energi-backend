82e935ee593aba5dfc27351684f28be0854239f6
amaliejacobsen <amajac@norlys.dk>
2026-05-20 07:50:15 +0200

    Update collector.py

--- collector.py
+++ collector.py
@@ -964,6 +964,13 @@ def collect_hydro_forecast_data():
 
 def collect_all():
     print(f"\n{'='*40}\nStart: {datetime.now()}\n{'='*40}")
+    collect_dk_data()
+    collect_hydro_data()
+    collect_nuclear_data()
+    collect_gas_data()
+    collect_capacity_data()
+    collect_consumption_data()
+    collect_dk_hourly_data()
     collect_hydro_forecast_data()
     print(f"\nFærdig: {datetime.now()}\n{'='*40}")
 
