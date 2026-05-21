0ea3711b4770d568ac2085003a2d374ba1102949
amaliejacobsen <amajac@norlys.dk>
2026-05-21 07:21:11 +0200

    Update collector.py

--- collector.py
+++ collector.py
@@ -1067,15 +1067,7 @@ def collect_temperature_forecast_data():
         print("Ingen data opsamlet.")
 def collect_all():
     print(f"\n{'='*40}\nStart: {datetime.now()}\n{'='*40}")
-    collect_dk_data()
-    collect_hydro_data()
-    collect_nuclear_data()
-    collect_gas_data()
-    collect_capacity_data()
-    collect_consumption_data()
     collect_dk_hourly_data()
-    collect_hydro_forecast_data()
-    collect_temperature_forecast_data()
     print(f"\nFærdig: {datetime.now()}\n{'='*40}")
 
 if __name__ == "__main__":
