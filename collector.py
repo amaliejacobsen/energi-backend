ce9f1b8236f5d9d49c58e8a05545da7364b420a3
amaliejacobsen <amajac@norlys.dk>
2026-06-08 12:42:05 +0200

    Update collector.py

--- collector.py
+++ collector.py
@@ -1005,22 +1005,18 @@ def collect_dk_hourly_data():
         
         for rec in fetch_all_records("Elspotprices", area, start=recent_start):
             dt_str = rec["HourDK"].replace('Z', '')
-            dt_dk = datetime.fromisoformat(dt_str)
-            dt_utc_str = (dt_dk - timedelta(hours=2)).strftime("%Y-%m-%dT%H:%M:%S")
-            price_dict[dt_utc_str] = {
+            price_dict[dt_str] = {
                 "area": area,
-                "datetime": dt_utc_str,
+                "datetime": dt_str,
                 "price_dkk": rec["SpotPriceDKK"]
             }
             
         for rec in fetch_all_records("DayAheadPrices", area, start=recent_start):
             dt_str = rec["TimeDK"].replace('Z', '')
-            dt_dk = datetime.fromisoformat(dt_str)
-            dt_utc_str = (dt_dk - timedelta(hours=2)).strftime("%Y-%m-%dT%H:%M:%S")
-            if dt_utc_str not in price_dict:
-                price_dict[dt_utc_str] = {
+            if dt_str not in price_dict:
+                price_dict[dt_str] = {
                     "area": area,
-                    "datetime": dt_utc_str,
+                    "datetime": dt_str,
                     "price_dkk": rec["DayAheadPriceDKK"]
                 }
         
@@ -1043,8 +1039,8 @@ def collect_dk_hourly_data():
         consumption_dict = {}
 
         for rec in fetch_all_records("ProductionConsumptionSettlement", area):
-            dt = datetime.fromisoformat(rec["HourDK"].replace('Z', ''))
-            dt_utc_str = (dt - timedelta(hours=2)).strftime("%Y-%m-%dT%H:%M:%S")
+            dt = datetime.fromisoformat(rec["HourDK"].replace('Z', '+00:00'))
+            dt_iso = dt.isoformat()
             solar    = (rec.get("SolarPowerLt10kW_MWh", 0) or 0) + \
                        (rec.get("SolarPowerGe10Lt40kW_MWh", 0) or 0) + \
                        (rec.get("SolarPowerGe40kW_MWh", 0) or 0)
@@ -1053,11 +1049,10 @@ def collect_dk_hourly_data():
             onshore  = (rec.get("OnshoreWindLt50kW_MWh", 0) or 0) + \
                        (rec.get("OnshoreWindGe50kW_MWh", 0) or 0)
             consumption = rec.get("GrossConsumption_MWh", 0) or 0
-            solar_dict[dt_utc_str]       = {"area": area, "source": "solar",       "datetime": dt_utc_str, "value_mwh": solar}
-            offshore_dict[dt_utc_str]    = {"area": area, "source": "offshore",    "datetime": dt_utc_str, "value_mwh": offshore}
-            onshore_dict[dt_utc_str]     = {"area": area, "source": "onshore",     "datetime": dt_utc_str, "value_mwh": onshore}
-            consumption_dict[dt_utc_str] = {"area": area, "source": "consumption", "datetime": dt_utc_str, "value_mwh": consumption}
-
+            solar_dict[dt_iso]       = {"area": area, "source": "solar",       "datetime": dt_iso, "value_mwh": solar}
+            offshore_dict[dt_iso]    = {"area": area, "source": "offshore",    "datetime": dt_iso, "value_mwh": offshore}
+            onshore_dict[dt_iso]     = {"area": area, "source": "onshore",     "datetime": dt_iso, "value_mwh": onshore}
+            consumption_dict[dt_iso] = {"area": area, "source": "consumption", "datetime": dt_iso, "value_mwh": consumption}
 
         for source, d in [("solar", solar_dict), ("offshore", offshore_dict), ("onshore", onshore_dict), ("consumption", consumption_dict)]:
             rows = list(d.values())
