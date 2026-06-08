2c865e5aef41571f08817d838b6251fc2ad36374
amaliejacobsen <amajac@norlys.dk>
2026-06-08 07:19:58 +0200

    Update collector.py

--- collector.py
+++ collector.py
@@ -1311,7 +1311,7 @@ def collect_realtid_dk_hourly():
 
 def collect_all():
     print(f"\n{'='*40}\nStart: {datetime.now()}\n{'='*40}")
-    collect_nuclear_data(
+    collect_nuclear_data()
     print(f"\nFærdig: {datetime.now()}\n{'='*40}")
 
 if __name__ == "__main__":
