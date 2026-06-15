fabb72055cb2b167bd8bd8317cac4fe1625075ea
amaliejacobsen <amajac@norlys.dk>
2026-06-15 07:41:06 +0200

    Update collector.py

--- collector.py
+++ collector.py
@@ -1004,7 +1004,9 @@ def collect_dk_hourly_data():
         recent_start = (current_date - timedelta(days=7)).strftime("%Y-%m-%d")
         
         for rec in fetch_all_records("Elspotprices", area, start=recent_start):
-            dt_str = rec["HourDK"].replace('Z', '')
+            dt_dk = datetime.fromisoformat(rec["HourDK"].replace('Z', ''))
+            dt_utc = dt_dk - timedelta(hours=2)
+            dt_str = dt_utc.strftime("%Y-%m-%dT%H:%M:%S")
             price_dict[dt_str] = {
                 "area": area,
                 "datetime": dt_str,
@@ -1012,7 +1014,9 @@ def collect_dk_hourly_data():
             }
             
         for rec in fetch_all_records("DayAheadPrices", area, start=recent_start):
-            dt_str = rec["TimeDK"].replace('Z', '')
+            dt_dk = datetime.fromisoformat(rec["TimeDK"].replace('Z', ''))
+            dt_utc = dt_dk - timedelta(hours=2)
+            dt_str = dt_utc.strftime("%Y-%m-%dT%H:%M:%S")
             if dt_str not in price_dict:
                 price_dict[dt_str] = {
                     "area": area,
